5dc97c0042dbae27353d0e04947629e3e6a5b520
pmichelotti <pmichelotti@citytechinc.com>
2015-09-25 09:36:45 -0500

    Sling angular variant JS

--- src/main/resources/SLING-INF/scripts/page/home/js/services/todoListService.js
+++ src/main/resources/SLING-INF/scripts/page/home/js/services/todoListService.js
@@ -2,15 +2,47 @@ angular.module( 'todo.services.todolist', [] )
     .service( 'TodoListService', function( $http ) {
 
         this.getTodoList = function( path ) {
-            return $http.get( path + '.model.json' )
+            return $http.get( path + '.2.json' )
                 .then( function( response ) {
-                    return response.data;
+                    var todoList = {
+                        path: path,
+                        items: [],
+                        itemsRemaining: 0
+                    };
+
+                    if ( response.data && response.data.items ) {
+                        for( var currentItemsKey in response.data.items ) {
+                            if ( response.data.items.hasOwnProperty( currentItemsKey ) && currentItemsKey.indexOf( 'item' ) === 0 ) {
+                                var currentItem = response.data.items[ currentItemsKey ];
+                                todoList.items.push( {
+                                    path: path + '/items/' + currentItemsKey,
+                                    title: currentItem.title,
+                                    done: currentItem.isDone
+                                } );
+
+                                if ( !currentItem.isDone ) {
+                                    todoList.itemsRemaining += 1;
+                                }
+                            }
+                        }
+                    }
+
+                    todoList.itemCount = todoList.items.length;
+
+                    return todoList;
                 } );
         };
 
         this.addTodoListItem = function( todoList, title ) {
 
-            return $http.post( todoList.path, { title: title } )
+            return $http.post( todoList.path + '/items/*', {
+                title: title,
+                "sling:resourceType": "todo/content/todolistitem",
+                "isDone": false,
+                "isDone@TypeHint": "Boolean",
+                ":nameHint": "item" + Date.now()
+                //TODO: Handle missing items path
+            } )
                 .then( function( response ) {
                     if ( response.status >= 400 ) {
                         throw new Error( 'Error creating new Item' );
@@ -21,7 +53,7 @@ angular.module( 'todo.services.todolist', [] )
 
         this.deleteTodoListItem = function( todoListItem ) {
 
-            return $http.delete( todoListItem.path )
+            return $http.post( todoListItem.path, { ":operation" : "delete" } )
                 .then( function( response ) {
                     if ( response.status >= 400 ) {
                         throw new Error( 'Error deleting Item ' + todoListItem.path );
@@ -32,7 +64,10 @@ angular.module( 'todo.services.todolist', [] )
 
         this.completeTodoListItem = function( todoListItem ) {
 
-            return $http.put( todoListItem.path, { done: true } )
+            return $http.post( todoListItem.path, {
+                isDone: true,
+                "isDone@TypeHint": "Boolean"
+            } )
                 .then( function( response ) {
                     if ( response.status >= 400 ) {
                         throw new Error( 'Error completing Item ' + todoListItem.path );
